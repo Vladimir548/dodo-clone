83590e70cd0bdd74558181abafa011a7c5462ea9
Vladimir548 <kormisenkovvladimir@gmail.com>
2024-09-04 13:50:26 +0400

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <main><Button variant={"outline"}>
+     df
+   </Button>
+       dsffffffffffffff
+   </main>
+  );
+}
