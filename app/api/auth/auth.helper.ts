39e05cb2443b7bbc3a7fc506b9f9864cd76a8159
Vladimir548 <kormisenkovvladimir@gmail.com>
2024-10-29 21:51:00 +0400

    delete method current user

--- app/api/auth/auth.helper.ts
+++ app/api/auth/auth.helper.ts
@@ -1,28 +1,12 @@
 import Cookies from 'js-cookie';
 
 import { EnumTokens } from './auth.service';
-import { jwtDecode } from 'jwt-decode';
-
-interface IDataToken {
-  userId: string;
-  cartId:string
-  iat: number;
-  exp: number;
-}
-
 
 export const getAccessToken = () => {
   const accessToken = Cookies.get(EnumTokens.ACCESS_TOKEN);
   return accessToken || null;
 };
 
-export const getIdUser = () => {
-  const accessToken = getAccessToken();
-  if (accessToken) {
-    const dataToken = jwtDecode<IDataToken>(accessToken);
-    return dataToken.userId;
-  }
-};
 export const saveTokenStorage = (accessToken: string) => {
   Cookies.set(EnumTokens.ACCESS_TOKEN, accessToken, {
     domain: 'localhost',
