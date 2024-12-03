7659aa4685b8dd7988098d59b13fbc1d85fb674d
Vladimir548 <kormisenkovvladimir@gmail.com>
2024-12-03 11:48:22 +0400

    add filter for product

--- components/shared/products-group-list/ProductGroup.tsx
+++ components/shared/products-group-list/ProductGroup.tsx
@@ -1,26 +1,48 @@
-"use client";
+'use client'
 
-import { QueryProduct } from "@/app/api/query-product";
-import ProductCard from "@/components/shared/products-group-list/ProductCard";
-import { Skeleton } from "@/components/ui/skeleton";
-import { useQuery } from "@tanstack/react-query";
+import { QueryProduct } from '@/app/api/query-product'
+import ProductCard from '@/components/shared/products-group-list/ProductCard'
+import { Skeleton } from '@/components/ui/skeleton'
+import { IFilterParams } from '@/interface/interface-filter-params'
+import { useCategoryStore } from '@/store/category'
+import { useFiltersStore } from '@/store/filters'
+import { useQuery } from '@tanstack/react-query'
 
 export default function ProductGroup({ id }: { id: number }) {
-  const { data, isPending } = useQuery({
-    queryKey: ["get-by-category", id],
-    queryFn: () => QueryProduct.byCategory(id),
-  });
-  if (isPending)
-    return (
-      <div className={"grid grid-cols-3 gap-4 "}>
-        <Skeleton count={9} className={"w-full dark:bg-primary h-[450px]"} />
-      </div>
-    );
-  return (
-    <div className={"grid grid-cols-3 gap-4 "}>
-      {data?.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
-  );
+	const activeSlug = useCategoryStore(state => state.activeCategory)
+
+	const { sizes, dough, priceFrom, priceTo, ingredients } = useFiltersStore(
+		state => ({
+			sizes: state?.sizes?.[activeSlug ?? ''],
+			dough: state?.dough,
+			priceTo: state.prices?.[activeSlug ?? '']?.priceTo,
+			priceFrom: state.prices?.[activeSlug ?? '']?.priceFrom,
+			ingredients: state.ingredients?.[activeSlug ?? ''],
+		})
+	)
+
+	const filter: IFilterParams = {
+		sizes,
+		dough: dough.map(val => val.value),
+		priceFrom,
+		priceTo,
+		ingredients,
+	}
+	const { data, isPending } = useQuery({
+		queryKey: ['get-by-category', id, filter],
+		queryFn: () => QueryProduct.byCategory(id, filter),
+	})
+	if (isPending)
+		return (
+			<div className={'grid grid-cols-3 gap-4 '}>
+				<Skeleton count={9} className={'w-full dark:bg-primary h-[450px]'} />
+			</div>
+		)
+	return (
+		<div className={'grid grid-cols-3 gap-4 '}>
+			{data?.map(product => (
+				<ProductCard key={product.id} product={product} />
+			))}
+		</div>
+	)
 }
