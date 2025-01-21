104f1548a18b214b520ae3d0382d352c7c0479fd
Vladimir548 <kormisenkovvladimir@gmail.com>
2025-01-21 20:11:43 +0400

    add check type product

--- components/cart/CartItemList.tsx
+++ components/cart/CartItemList.tsx
@@ -2,7 +2,9 @@
 
 import CartItemCard from '@/components/cart/CartItemCard'
 import { Skeleton } from '@/components/ui/skeleton'
+import { TypeProduct } from '@/interface/enums'
 import { ICartItemResponse } from '@/interface/interface-cart-item'
+import CartItemCardHalf from './CartItemCardHalf'
 
 export default function CartItemList({
 	items,
@@ -11,13 +13,22 @@ export default function CartItemList({
 	items: ICartItemResponse
 	isPending: boolean
 }) {
-	console.log('items', items)
 	return (
 		<ul className={'flex flex-col gap-y-2 '}>
 			{!isPending ? (
 				<>
-					{items?.map(item => (
-						<CartItemCard key={item.id} data={item} />
+					{items?.map((item, index) => (
+						<li
+							key={index}
+							className='shadow-sm-card-dark rounded-md px-2 py-1 hover:shadow-sm-card-dark-inner cursor-pointer duration-300 ease-linear'
+						>
+							{item.product.type === TypeProduct.PIZZA_HALF && (
+								<CartItemCardHalf key={item.id} data={item} />
+							)}
+							{item.product.type !== TypeProduct.PIZZA_HALF && (
+								<CartItemCard key={item.id} data={item} />
+							)}
+						</li>
 					))}
 				</>
 			) : (
