a2f77e1af584f7003db3133ece2c357805e3be9a
Vladimir548 <kormisenkovvladimir@gmail.com>
2025-01-21 19:59:00 +0400

    add url variant

--- data/dashboard-nav.ts
+++ data/dashboard-nav.ts
@@ -1,31 +1,36 @@
-import {Carrot, IconNode, LayoutGrid, Pizza, Ruler} from "lucide-react";
-
+import { Carrot, IconNode, LayoutGrid, Pizza, Ruler } from 'lucide-react'
 
 interface IDashboardNav {
-    label:string,
-    url:string,
-    icon:IconNode
-
+	label: string
+	url: string
+	icon: IconNode
 }
 
 export const DASHBOARDNAV = [
-    {
-        label: "Категории",
-        url: "/dashboard/category",
-        icon: LayoutGrid,
-    },
-    {
-        label: "Ингредиенты",
-        url: "/dashboard/ingredient",
-        icon: Carrot,
-    },
-    {
-        label: "Продукты",
-        url: "/dashboard/product",
-        icon: Pizza ,
-    },  {
-        label: "Параметры",
-        url: "/dashboard/parameter",
-        icon: Ruler ,
-    },
+	{
+		label: 'Категории',
+		url: '/dashboard/category',
+		icon: LayoutGrid,
+	},
+	{
+		label: 'Ингредиенты',
+		url: '/dashboard/ingredient',
+		icon: Carrot,
+	},
+	{
+		label: 'Продукты',
+		url: '/dashboard/product',
+		icon: Pizza,
+	},
+	{
+		label: 'Параметры',
+		url: '/dashboard/parameter',
+		icon: Ruler,
+	},
+
+	{
+		label: 'Варианты',
+		url: '/dashboard/variant',
+		icon: Ruler,
+	},
 ]
