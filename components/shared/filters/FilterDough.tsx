cc62dc4cb22c130a475a4e4ecc465af1c644b98d
Vladimir548 <kormisenkovvladimir@gmail.com>
2025-01-21 20:10:26 +0400

    adding a request to the server

--- components/shared/filters/FilterDough.tsx
+++ components/shared/filters/FilterDough.tsx
@@ -1,23 +1,29 @@
 'use client'
 
+import { QueryVariant } from '@/app/api/query-variant'
 import { FiltersListCheckbox } from '@/components/shared/filters/FiltersListChecbox'
-import { DATADOUGHTYPE } from '@/data/dough-type'
+import { TypeProduct } from '@/interface/enums'
 import { useFiltersStore } from '@/store/filters'
+import { useQuery } from '@tanstack/react-query'
 
 export default function FilterDough() {
 	const { doughs, toggleDough } = useFiltersStore(state => ({
 		doughs: state.dough,
 		toggleDough: (id: number) => state?.toggleDough(id),
 	}))
+	const { data } = useQuery({
+		queryKey: ['variant-by-type'],
+		queryFn: () => QueryVariant.byType(TypeProduct.PIZZA),
+	})
 
-	const item = DATADOUGHTYPE.map(dough => ({
+	const item = data?.map(dough => ({
 		value: String(dough.id),
-		text: dough.name,
+		text: dough.value,
 	}))
 	return (
 		<div className={'py-4 border-b'}>
 			<FiltersListCheckbox
-				selected={doughs.map(val => val.id)}
+				selected={doughs}
 				onClickCheckbox={toggleDough}
 				name={'dough'}
 				title={'Тесто'}
